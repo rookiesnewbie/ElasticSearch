94e8b67ea6f82a7c54a463b0d5210b7e21712906
rookiesnewbie <2049448867@qq.com>
2023-11-13 15:12:42 +0800

    查询文档

---
+++ src/main/java/com/es/ESTest_Search_Doc.java
@@ -0,0 +1,42 @@
+package com.es;
+
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+
+/**
+ * @Author LiTeng
+ * @Date 2023/11/13 13:49
+ * Version 1.0
+ * @Description 查询文档
+ */
+public class ESTest_Search_Doc {
+    public static void main(String[] args) throws IOException {
+        //创建es客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost",9200,"http"))
+        );
+
+        //查询文档
+        GetRequest request = new GetRequest().index("user").id("1001");
+
+        GetResponse response = esClient.get(request, RequestOptions.DEFAULT);
+        //响应状态
+        System.out.println(response.getSourceAsString());
+
+        //关闭客户端
+        esClient.close();
+    }
+
+}
