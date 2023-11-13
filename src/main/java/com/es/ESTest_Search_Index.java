d991e0d2a667e90cc1a65842104c36679f363922
rookiesnewbie <2049448867@qq.com>
2023-11-13 14:25:03 +0800

    查询索引

---
+++ src/main/java/com/es/ESTest_Search_Index.java
@@ -0,0 +1,40 @@
+package com.es;
+
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+
+import java.io.IOException;
+
+/**
+ * @Author LiTeng
+ * @Date 2023/11/13 13:49
+ * Version 1.0
+ * @Description 查询索引
+ */
+public class ESTest_Search_Index {
+    public static void main(String[] args) throws IOException {
+        //创建es客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost",9200,"http"))
+        );
+
+        //创建索引
+        GetIndexRequest user = new GetIndexRequest("user"); //索引名必须是小写
+        GetIndexResponse getIndexResponse = esClient.indices().get(user, RequestOptions.DEFAULT);
+
+        //响应状态
+        System.out.println(getIndexResponse.getAliases());
+        System.out.println(getIndexResponse.getMappings());
+        System.out.println(getIndexResponse.getSettings());
+        //关闭客户端
+        esClient.close();
+    }
+
+}
